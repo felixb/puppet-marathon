8b0a95d41fe3fea54cd1c79945bd065acba3e15d
Felix Bechstein <felix.bechstein@otto.de>
2016-01-21 14:25:35 +0100

    Support setting secrets
    This allows to set up framework authentication

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,22 +1,25 @@
 # == Class: marathon::config
 #
 class marathon::config(
-  $conf_dir_base = '/etc/marathon',
-  $conf_dir_name = 'conf',
-  $owner         = 'root',
-  $group         = 'root',
-  $master        = undef,
-  $zookeeper     = undef,
-  $options       = {},
-  $env_var       = {},
-  $manage_logger = true,
-  $logger        = 'logback',
-  $log_dir       = '/var/log/marathon',
-  $log_filename  = 'marathon.log',
-  $log_level     = 'info',
-  $ulimit        = undef,
-  $java_home     = undef,
-  $java_opts     = '-Xmx512m',
+  $conf_dir_base                    = '/etc/marathon',
+  $conf_dir_name                    = 'conf',
+  $owner                            = 'root',
+  $group                            = 'root',
+  $master                           = undef,
+  $zookeeper                        = undef,
+  $options                          = {},
+  $env_var                          = {},
+  $manage_logger                    = true,
+  $logger                           = 'logback',
+  $log_dir                          = '/var/log/marathon',
+  $log_filename                     = 'marathon.log',
+  $log_level                        = 'info',
+  $ulimit                           = undef,
+  $mesos_auth_principal             = undef,
+  $mesos_auth_secret                = undef,
+  $mesos_auth_secret_file           = '/etc/marathon/.secret',
+  $java_home                        = undef,
+  $java_opts                        = '-Xmx512m',
 ) {
   $conf_dir = "${conf_dir_base}/${conf_dir_name}"
   file { [$conf_dir_base, $conf_dir]:
@@ -24,6 +27,23 @@ class marathon::config(
     owner  => $owner,
     group  => $group,
   }
+
+  if ($mesos_auth_principal != undef and $mesos_auth_secret != undef) {
+    $secret_options = {
+      'mesos_authentication_principal'   => $mesos_auth_principal,
+      'mesos_authentication_secret_file' => $mesos_auth_secret_file,
+    }
+    $configure_secrets = true
+  } elsif ($options['mesos_authentication_principal'] != undef and $mesos_auth_secret != undef) {
+    $secret_options = {
+      'mesos_authentication_secret_file' => $mesos_auth_secret_file,
+    }
+    $configure_secrets = true
+  } else {
+    $secret_options = {}
+    $configure_secrets = false
+  }
+  $real_options = merge($secret_options, $options)
 
   if $master {
     mesos::property { 'marathon_master':
@@ -44,7 +64,7 @@ class marathon::config(
   }
 
   create_resources(mesos::property,
-    mesos_hash_parser($options, 'marathon'),
+    mesos_hash_parser($real_options, 'marathon'),
     {
       dir     => $conf_dir,
       service => undef,
@@ -84,5 +104,18 @@ class marathon::config(
     owner   => $owner,
     group   => $group,
     mode    => '0644',
+  }
+
+  if ($configure_secrets) {
+    $real_mesos_auth_secret_file =  $real_options['mesos_authentication_secret_file']
+    validate_absolute_path($real_mesos_auth_secret_file)
+
+    file { $real_mesos_auth_secret_file:
+      ensure  => file,
+      content => $mesos_auth_secret,
+      owner   => $owner,
+      group   => $group,
+      mode    => '0400',
+    }
   }
 }
